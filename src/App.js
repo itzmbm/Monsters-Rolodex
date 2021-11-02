5a7b5b2beae6d5a3db60873f5059918d989fa792
Manjunath B M <manjunathbm438@gmail.com>
2021-11-03 00:10:30 +0530

    search added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -15,11 +17,23 @@ class App extends Component {
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
-
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
